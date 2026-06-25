586cf4f3d92bfb92440d8228cd6a2a77bbf6fe4f
Daniel <danielariassolano@gmail.com>
2026-06-25 16:45:24 -0600

    se programo la clase deportes
    se programo  Verificar si una disciplina tiene espacios disponibles y Determinar si una disciplina alcanzó su capacidad máxima

--- src/deporte/Deportes.java
+++ src/deporte/Deportes.java
@@ -15,6 +15,7 @@ public class Deportes {
     private String Voleibol;
     private Categoria Categoria;
     private int capacidadMaxima;
+    private int inscritosActuales;
     
     public String getFutbol(){
         return Futbol;
@@ -26,24 +27,34 @@ public class Deportes {
         return Voleibol;
     }
     public int getCapacidadMaxima(){
-    return capacidadMaxima;
+        return capacidadMaxima;
+    }
+    public int getInscritosActuales(){
+        return inscritosActuales;
     }
     
     public void setCapacidadMaxima(int capacidadMaxima){
-    this.capacidadMaxima = capacidadMaxima;
-    }
-   
-    
-    public Deportes (String Futbol, String Baloncesto, String Voleibol,int capacidadMaxima){
-        this.Futbol=Futbol;
-        this.Baloncesto=Baloncesto;
-        this.Voleibol=Voleibol;
         this.capacidadMaxima = capacidadMaxima;
+    }
+    
+    public Deportes (String Futbol, String Baloncesto, String Voleibol, int capacidadMaxima){
+        this.Futbol = Futbol;
+        this.Baloncesto = Baloncesto;
+        this.Voleibol = Voleibol;
+        this.capacidadMaxima = capacidadMaxima;
+        this.inscritosActuales = 0;
     }
     
     @Override
     public String toString(){
-        return "Futbol"+this.getFutbol()+ "\nBaloncesto"+ this.getBaloncesto()+ "\nVoleibol"+this.getVoleibol()+
-                "\nCapacidad Maxima"+this.getCapacidadMaxima();
+        return "Futbol"+this.Futbol+ "\nBaloncesto"+ Baloncesto+ "\nVoleibol"+Voleibol+"\nCapacidad Maxima"+capacidadMaxima;
+    }
+    
+    public boolean tieneEspaciosDisponibles(){
+        return inscritosActuales < capacidadMaxima;
+    }
+    
+    public boolean alcanzoCapacidadMaxima(){
+        return inscritosActuales >= capacidadMaxima;
     }
 }
