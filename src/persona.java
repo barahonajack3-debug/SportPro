80c93ea3cf3f3dcdd00a6d25062ea373d32eb53e
JaxanderBC <barahonajack3@gmail.com>
2026-06-25 14:26:02 -0600

    Se creo la clase Persona
    Se crearon sus metodos get, set, constructor y toString

---
+++ src/persona.java
@@ -0,0 +1,49 @@
+
+import java.time.LocalDate;
+import java.time.Period;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class persona {
+    //Atributos 
+    private String Nombre_Completo;
+    private LocalDate Fecha_Nacimiento;
+    private String Telefono;
+
+    //Metodos get
+    public String getNombre_Completo() {
+        return Nombre_Completo;
+    }
+    public LocalDate getFecha_Nacimiento() {
+        return Fecha_Nacimiento;
+    }
+    public String getTelefono() {
+        return Telefono;
+    }
+    public int getEdad(){
+        return Period.between(Fecha_Nacimiento,LocalDate.now()).getYears();
+    }
+    //Metodos Set
+    public void setNombre_Completo(String Nombre_Completo) {
+        this.Nombre_Completo = Nombre_Completo;
+    }
+    //Metodo Constructor
+    public persona(String Nombre_Completo, LocalDate Fecha_Nacimiento, String Telefono) {
+        this.Nombre_Completo = Nombre_Completo;
+        this.Fecha_Nacimiento = Fecha_Nacimiento;
+        this.Telefono = Telefono;
+    }
+    //Metodo ToString
+    @Override
+    public String toString() {
+        return "Nombre_Complet:" + Nombre_Completo + "\nFecha_Nacimiento:" + Fecha_Nacimiento + "'\nTelefono:" + Telefono;
+    }
+    
+}
