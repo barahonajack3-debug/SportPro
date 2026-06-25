a93085d01faf4af02ba61194cfb5bfd225590824
JaxanderBC <barahonajack3@gmail.com>
2026-06-25 15:49:58 -0600

    Se modifico la clase displina

--- src/academia/Academias.java
+++ src/academia/Academias.java
@@ -10,42 +10,49 @@ package academia;
  */
 public class Academias {
     //Atributos
-    private String Disciplinas; //Debo importar de disciplina
-    private String Entrenadores; //Debo importar de entrenador
-    private String Participantes; //Debo importar de participante
-    private String Sistema; //Debo importar de sistema
-    //Metodos get 
-    public String getDisciplinas() {
+    private String Nombre;
+    private String[] Disciplinas; //Debo importar de disciplina
+    private int cantidadDisciplinas;
+    //Metodos get
+    public String getNombre() {
+        return Nombre;
+    }
+    public String[] getDisciplinas() {
         return Disciplinas;
     }
-    public String getEntrenadores() {
-        return Entrenadores;
-    }
-    public String getParticipantes() {
-        return Participantes;
-    }
-    public String getSistema() {
-        return Sistema;
+    public int getCantidadDisciplinas() {
+        return cantidadDisciplinas;
     }
     //Metodos set
-    public void setDisciplinas(String Disciplinas) {
-        this.Disciplinas = Disciplinas;
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre; 
     }
-    public void setEntrenadores(String Entrenadores) {
-        this.Entrenadores = Entrenadores;
-    }
-    public void setParticipantes(String Participantes) {
-        this.Participantes = Participantes;
+    public void setCantidadDisciplinas(int cantidadDisciplinas) {
+        this.cantidadDisciplinas = cantidadDisciplinas;
     }
     //Metodo Constructor
-    public Academias(String Disciplinas, String Entrenadores, String Participantes, String Sistema) {
-        this.Disciplinas = Disciplinas;
-        this.Entrenadores = Entrenadores;
-        this.Participantes = Participantes;
-        this.Sistema = Sistema;
+       public Academias(String nombre, int capacidadMaxDisciplinas) {
+        this.Nombre=nombre;
+        this.Disciplinas=new Disciplinas[capacidadMaxDisciplinas];
+        this.cantidadDisciplinas=0;
     }
+       public Academias(String nombre) {
+        this(nombre, 10);
+    }
+    //Metodos Funciones
+       //Registrar entrda de disciplinas 
+       public boolean registrarDisciplina(Disciplina d){
+       if (cantidadDisciplinas >= disciplinas.length) {
+       System.out.println("No hay espacios disponibles");
+       return false;
+       }
+           Disciplinas[cantidadDisciplinas]=d;
+           cantidadDisciplinas++;
+           return true;
+       }
+       
     //To String
     public String toString() {
-        return "Disciplinas:" + Disciplinas + "\nEntrenadores:" + Entrenadores + "\nParticipantes:" + Participantes + "\nSistema:" + Sistema;
+        return "Disciplinas:" + Disciplinas;
     }
 }
